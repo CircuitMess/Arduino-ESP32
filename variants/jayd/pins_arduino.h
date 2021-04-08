d5f2774e18892aa6a10b43ed1f464b1054eb38af
Filip Budiša <filip@circuitmess.com>
2021-04-08 17:45:43 +0200

    Add Jay-D with NoOTA partition scheme

---
+++ variants/jayd/pins_arduino.h
@@ -0,0 +1,14 @@
+#ifndef Pins_Arduino_h
+#define Pins_Arduino_h
+
+#include <stdint.h>
+
+static const uint8_t SDA = 26;
+static const uint8_t SCL = 27;
+
+static const uint8_t SCK = 18;
+static const uint8_t MISO = 19;
+static const uint8_t MOSI = 23;
+static const uint8_t SS = -1;
+
+#endif /* Pins_Arduino_h */
