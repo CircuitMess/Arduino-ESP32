c63698146219d940c411815ddb5e52b70b0c865d
Filip Budiša <filip@circuitmess.com>
2021-09-16 13:58:31 +0200

    Add LovyanGFX library

---
+++ libraries/LovyanGFX/src/lgfx/v1/platforms/device.hpp
@@ -0,0 +1,60 @@
+/*----------------------------------------------------------------------------/
+  Lovyan GFX - Graphics library for embedded devices.
+
+Original Source:
+ https://github.com/lovyan03/LovyanGFX/
+
+Licence:
+ [FreeBSD](https://github.com/lovyan03/LovyanGFX/blob/master/license.txt)
+
+Author:
+ [lovyan03](https://twitter.com/lovyan03)
+
+Contributors:
+ [ciniml](https://github.com/ciniml)
+ [mongonta0716](https://github.com/mongonta0716)
+ [tobozo](https://github.com/tobozo)
+/----------------------------------------------------------------------------*/
+#pragma once
+
+#if defined (ESP_PLATFORM)
+
+ #include <sdkconfig.h>
+
+ #if defined (CONFIG_IDF_TARGET_ESP32C3)
+
+  #include "esp32c3/Bus_SPI.hpp"
+
+ #else
+
+  #include "esp32/Light_PWM.hpp"
+  #include "esp32/Bus_SPI.hpp"
+  #include "esp32/Bus_I2C.hpp"
+  #include "esp32/Bus_Parallel8.hpp"
+
+ #endif
+
+#elif defined ( ESP8266 )
+
+#include "esp8266/Bus_SPI.hpp"
+
+#elif defined (__SAMD21__)
+
+#include "samd21/Bus_SPI.hpp"
+#include "samd21/Bus_I2C.hpp"
+
+#elif defined (__SAMD51__)
+
+#include "samd51/Bus_SPI.hpp"
+#include "samd51/Bus_I2C.hpp"
+
+#elif defined (STM32F2xx) || defined (STM32F4xx) || defined (STM32F7xx)
+
+#include "stm32/Bus_SPI.hpp"
+
+#elif defined ( ARDUINO )
+
+#include "arduino_default/Bus_SPI.hpp"
+
+#endif
+
