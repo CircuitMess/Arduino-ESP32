fcceae8a7b80ffde5fde8aed3a8a121c63f810e2
Filip Budiša <filip@circuitmess.com>
2022-09-28 15:17:58 +0200

    Enable UNSCII_8 lvgl font

--- libraries/lvgl/src/lv_conf.h
+++ libraries/lvgl/src/lv_conf.h
@@ -331,7 +331,7 @@ e.g. "stm32f769xx.h" or "stm32f429xx.h"*/
 #define LV_FONT_SIMSUN_16_CJK            0  /*1000 most common CJK radicals*/
 
 /*Pixel perfect monospace fonts*/
-#define LV_FONT_UNSCII_8  0
+#define LV_FONT_UNSCII_8  1
 #define LV_FONT_UNSCII_16 0
 
 /*Optionally declare custom fonts here.
