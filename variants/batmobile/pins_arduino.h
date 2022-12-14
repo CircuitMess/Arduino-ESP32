366490f4e37264f39b1bfc724211cf2afcbb3352
Emil Gajšak <emil.gajsak@gmail.com>
2022-12-14 14:26:53 +0100

    Added pins_arduino.h for Batmobile board

---
+++ variants/batmobile/pins_arduino.h
@@ -0,0 +1,18 @@
+#ifndef Pins_Arduino_h
+#define Pins_Arduino_h
+
+#include <stdint.h>
+
+#define analogInputToDigitalPin(p)  (((p)<20)?(esp32_adc2gpio[(p)]):-1)
+#define digitalPinToInterrupt(p)    (((p)<40)?(p):-1)
+#define digitalPinHasPWM(p)         (p < 34)
+
+static const uint8_t SDA = -1;
+static const uint8_t SCL = -1;
+
+static const uint8_t SCK = -1;
+static const uint8_t MISO = -1;
+static const uint8_t MOSI = -1;
+static const uint8_t SS = 15;
+
+#endif /* Pins_Arduino_h */
